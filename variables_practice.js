0f491bc5f256c23226ee0ccaa4cde342f75a7170
Jane Akinyi Nyasoro <janenyasoro@gmail.com>
2026-04-02 07:17:00 +0300

    Converting temperatures

---
+++ variables_practice.js
@@ -0,0 +1,14 @@
+const locationName = "Green Valley";
+let temperatureReading = 25.3;
+let isVerified = "false";
+
+
+// Variable reassignment
+temperatureReading = 26.1;
+
+isVerified = true
+
+//  Debugging
+console.log(temperatureReading); // 26.1
+
+console.log(isVerified); // true
