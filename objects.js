1adbabf26cb35aad46d128a377273e25258d9040
Jane Akinyi Nyasoro <janenyasoro@gmail.com>
2026-04-13 11:23:01 +0300

    Javascript modules using spread operator

---
+++ objects.js
@@ -0,0 +1,7 @@
+let menuItem= {
+    name: "Margherita Pizza",
+    price: 12.99,
+    isAvailable: true
+}
+console.log(menuItem);
+console.log(menuItem.name);
---
+++ productManagement.js
@@ -0,0 +1,7 @@
+let product = {
+    name: "Wireless Mouse",
+    price: 29.99,
+    stock: 100
+}
+console.log(product.name);
+console.log(product["price"]);
